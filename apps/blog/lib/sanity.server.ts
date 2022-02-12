1f6d76e4152b59d065af10223ba565d1696e2a94
zxyz <cmpxchg32b@protonmail.com>
2022-02-12 23:51:40 +0200

    typescript

--- apps/blog/lib/sanity.server.ts
+++ apps/blog/lib/sanity.server.ts
@@ -3,14 +3,14 @@ import { sanityConfig as config } from "./config";
 import type { SanityClient } from "@sanity/client";
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient: SanityClient = createClient(config);
+export const sanityClient = createClient(config) as unknown as SanityClient;
 
 // Set up a preview client with serverless authentication for drafts
-export const previewClient: SanityClient = createClient({
+export const previewClient = createClient({
   ...config,
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
-});
+}) as unknown as SanityClient;
 
 // Helper function for easily switching between normal client and preview client
 export const getClient = (usePreview = false) =>
