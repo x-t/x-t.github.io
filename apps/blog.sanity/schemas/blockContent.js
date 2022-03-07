1cbba6c56d59eef7116bd3bf4afc530f6d28c49b
zxyz <cmpxchg32b@protonmail.com>
2022-03-07 23:27:24 +0000

    sanity

--- apps/blog.sanity/schemas/blockContent.js
+++ apps/blog.sanity/schemas/blockContent.js
@@ -67,10 +67,10 @@ export default {
           type: "text",
           name: "alt",
           title: "Alternative text",
-          description: `Some of your visitors cannot see images, 
-            be they blind, color-blind, low-sighted; 
-            alternative text is of great help for those 
-            people that can rely on it to have a good idea of 
+          description: `Some of your visitors cannot see images,
+            be they blind, color-blind, low-sighted;
+            alternative text is of great help for those
+            people that can rely on it to have a good idea of
             what\'s on your page.`,
           options: {
             isHighlighted: true,
@@ -81,6 +81,9 @@ export default {
     },
     {
       type: "code",
+    },
+    {
+      type: "youtube",
     },
   ],
 };
