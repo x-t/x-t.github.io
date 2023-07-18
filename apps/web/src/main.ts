b79747a0ea9ece6748e3a60ce9ebf0321e14f658
zxyz <zxyz@zxyzs-MBP.lan>
2023-07-18 04:49:49 +0300

    broken frontend

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -6,7 +6,6 @@ import "./style.css";
 
 declare global {
   interface Window {
-    blog_callback: (posts: any) => void;
     Sentry: typeof Sentry;
     Alpine: typeof Alpine;
     botdPromise: Promise<any>;
@@ -22,7 +21,6 @@ Sentry.init({
 window.endpoints = {
   fetch: import.meta.env.VITE_FETCH_ENDPOINT,
   form: import.meta.env.VITE_FORM_ENDPOINT,
-  blog: import.meta.env.VITE_BLOG_ENDPOINT,
 };
 
 window.Alpine = Alpine;
