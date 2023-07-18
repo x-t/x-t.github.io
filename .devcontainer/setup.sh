b8f201f0eb7c11bf73f8cbeab9d1e6ae3790d826
zxyz <cmpxchg32b@protonmail.com>
2023-07-19 00:56:29 +0300

    cloud dev updates (untested)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,10 +3,5 @@
 set -ex
 
 sudo corepack enable
-docker run --name guestbook -e POSTGRES_PASSWORD=password -e POSTGRES_DB=guestbook -d -p 5432:5432 postgres
 pnpm install
-printf 'su postgres\npsql -d guestbook\nCREATE EXTENSION IF NOT EXISTS "uuid-ossp";\n' | docker exec -i guestbook /bin/bash
-export DATABASE_URL="postgres://postgres:password@localhost:5432/guestbook"
-pnpm run prisma:push --filter guestbook-api
-pnpm run prisma:seed --filter guestbook-api
 echo $DOPPLER_SERVICE_TOKEN | doppler configure set token --scope /
